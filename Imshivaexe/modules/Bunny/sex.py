c5ec988bebba81fcc917729a6ff2ace0e251ee75
Shiva Mishra <126594334+Imshivaexe@users.noreply.github.com>
2024-01-08 11:35:40 +0530

    Update sex.py

--- Imshivaexe/modules/Bunny/sex.py
+++ Imshivaexe/modules/Bunny/sex.py
@@ -2,3 +2,39 @@ from pyrogram import Client, filters
 from config import HANDLER as hl
 from Imshivaexe import Bunny
 from pyrogram.types import Message
+import asyncio
+
+
+@Bunny.on_message(filters.command(["sex", "sux"], hl) & filters.me)
+async def hearts(client: Client, message: Message):
+    await message.edit("🤵‍♂              👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂        👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂   👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂👼👰‍♀")
+    await asyncio.sleep(3)
+    await message.edit("🤵‍♂              👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂        👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂   👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂👼👰‍♀")
+    await asyncio.sleep(3)
+    await message.edit("🤵‍♂              👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂        👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂   👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂👼👰‍♀")
+    await asyncio.sleep(3)
+    await message.edit("🤵‍♂              👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂        👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂   👰‍♀")
+    await asyncio.sleep(0.5)
+    await message.edit("🤵‍♂👼👰‍♀")
